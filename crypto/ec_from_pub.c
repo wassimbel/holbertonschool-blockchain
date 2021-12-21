471a1f8908e493117630d1e27cdf877275f3f910
wassimbel <wassim.belhedi1@gmail.com>
2021-12-21 02:00:47 +0000

    fix

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -19,11 +19,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	group = EC_GROUP_new_by_curve_name(EC_CURVE);
 	if (!group)
 		return (NULL);
-
 	p = EC_POINT_new(group);
 	if (!p)
 	{
-		EC_GROUP_free(group);
 		EC_GROUP_free(group);
 		return (NULL);
 	}

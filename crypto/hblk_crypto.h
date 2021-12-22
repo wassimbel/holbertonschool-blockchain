0e80718ac372a670128985a1071638737cb511e3
wassimbel <wassim.belhedi1@gmail.com>
2021-12-22 18:03:49 +0000

    fix betty

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -47,5 +47,5 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-                        sig_t *sig);
+		sig_t *sig);
 #endif /* _HBLK_CRYPTO_H_ */

d19841b72d80d60a4bd9dccede39d611f405e250
wassimbel <wassim.belhedi1@gmail.com>
2021-12-22 15:36:13 +0000

    flip pub and pri

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -17,19 +17,19 @@ EC_KEY *ec_load(char const *folder)
 	if (stat(folder, &sb) == -1)
 		return (NULL);
 
-	sprintf(file, "./%s/%s", folder, PRI_FILENAME);
-	fp = fopen(file, "r");
-	if (!fp)
-		return (NULL);
-	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-		return (NULL);
-	fclose(fp);
-
 	sprintf(file, "./%s/%s", folder, PUB_FILENAME);
 	fp = fopen(file, "r");
 	if (!fp)
 		return (NULL);
 	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+		return (NULL);
+	fclose(fp);
+
+	sprintf(file, "./%s/%s", folder, PRI_FILENAME);
+	fp = fopen(file, "r");
+	if (!fp)
+		return (NULL);
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
 		return (NULL);
 	fclose(fp);
 	return (key);

3b2c804301bf616fda313cc3d4772240a05479c8
wassimbel <wassim.belhedi1@gmail.com>
2021-12-23 00:43:32 +0000

    1 valid 0 invalid -1 error

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -16,8 +16,8 @@ int ec_verify(EC_KEY const *key, uint8_t const *msg,
 	if (!key || !msg || !sig)
 		return (0);
 
-	if (!ECDSA_verify(0, msg, (int)msglen, sig->sig,
-				sig->len, (EC_KEY *)key))
+	if (ECDSA_verify(0, msg, (int)msglen, sig->sig,
+				sig->len, (EC_KEY *)key) < 1)
 		return (0);
 	return (1);
 }

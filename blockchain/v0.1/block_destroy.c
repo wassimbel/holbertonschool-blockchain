76018cbb5c6c0480f016b732120afaf879ac6f87
wassimbel <wassim.belhedi1@gmail.com>
2021-12-27 23:51:46 +0000

    deletes a block

---
+++ blockchain/v0.1/block_destroy.c
@@ -0,0 +1,11 @@
+#include "blockchain.h"
+
+/**
+ * block_destroy - deletes an existing block
+ * @block: points to the block to delete
+ **/
+
+void block_destroy(block_t *block)
+{
+	free(block);
+}

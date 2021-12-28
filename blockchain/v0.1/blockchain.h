e202fd7eaf63e6f114765b8106efecd839c58391
wassimbel <wassim.belhedi1@gmail.com>
2021-12-28 00:04:37 +0000

    deletes an existing blockchain along with all the blocks

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -83,4 +83,5 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 			uint32_t data_len);
 
 void block_destroy(block_t *block);
+void blockchain_destroy(blockchain_t *blockchain);
 #endif /* _BLOCKCHAIN_H_ */

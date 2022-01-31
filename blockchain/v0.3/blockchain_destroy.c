d07eb4a0fe14170b5c63e9dc7da9f3556fa4b595
wassimbel <wassim.belhedi1@gmail.com>
2022-01-31 04:47:18 +0000

    fix

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -10,7 +10,7 @@ void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
 		return;
-	llist_destroy(blockchain->chain, 1, NULL);
+	llist_destroy(blockchain->chain, 1, (void (*)(llist_node_t))block_destroy);
 	llist_destroy(blockchain->unspent, 1, NULL);
 	free(blockchain);
 }
